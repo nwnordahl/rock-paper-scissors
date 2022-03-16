013f532821c341aa20264757098024ab76de91a7
nwnordahl <nicolaiwnordahl@gmail.com>
2022-03-16 13:55:27 +0100

    Create main game function and refactor some code

--- script.js
+++ script.js
@@ -9,35 +9,74 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  const playerSelectionUpperCase = playerSelection.toUpperCase();
-
   //   Check if computer win
   if (
-    (playerSelectionUpperCase === "ROCK" && computerSelection === "PAPER") ||
-    (playerSelectionUpperCase === "PAPER" &&
-      computerSelection === "SCISSORS") ||
-    (playerSelectionUpperCase === "SCISSORS" && computerSelection === "ROCK")
+    (playerSelection === "ROCK" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "ROCK")
   ) {
-    return `You lose! ${computerSelection} beats ${playerSelectionUpperCase}`;
+    return "Computer won";
   }
 
   //   Check if player win
   else if (
-    (computerSelection === "ROCK" && playerSelectionUpperCase === "PAPER") ||
-    (computerSelection === "PAPER" &&
-      playerSelectionUpperCase === "SCISSORS") ||
-    (computerSelection === "SCISSORS" && playerSelectionUpperCase === "ROCK")
+    (computerSelection === "ROCK" && playerSelection === "PAPER") ||
+    (computerSelection === "PAPER" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "ROCK")
   ) {
-    return `You win! ${playerSelectionUpperCase} beats ${computerSelection}`;
+    return "Player won";
   }
 
   //   It's  draw
   else {
-    return `Computer chose ${computerSelection} too, it's a draw!`;
+    return "Draw";
   }
 }
 
-// Test to see if it works
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// Main game function
+function game() {
+  const playRounds = 5;
+  let playerScore = 0;
+  let computerScore = 0;
+
+  //   Here's where the fun part begins!
+  for (let i = 0; i < playRounds; i++) {
+    const playerSelection = prompt(
+      "Enter rock, paper, or scissors:"
+    ).toUpperCase();
+    const computerSelection = computerPlay();
+    const playRoundResult = playRound(playerSelection, computerSelection);
+
+    // Check who won the round
+    if (playRoundResult === "Computer won") {
+      computerScore++;
+      console.log(
+        `Computer win! ${computerSelection} beats ${playerSelection}.`
+      );
+    }
+    if (playRoundResult === "Player won") {
+      playerScore++;
+      console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+    } else {
+      console.log(`Computer chose ${computerSelection} too, it's a draw!`);
+    }
+  }
+
+  //   Play summary
+  if (playerScore < computerScore) {
+    console.log(
+      `Computer got the best score with a score of ${computerScore} out of ${playRounds}.`
+    );
+  } else if (playerScore > computerScore) {
+    console.log(
+      `You got the best score with a score of ${playerScore} out of ${playRounds}.`
+    );
+  } else {
+    console.log(
+      `You both got a score of ${playerScore} out of ${playRounds}, it's a draw!`
+    );
+  }
+}
+
+// Let there be light!
+game();
