3d7ee0c0057fb7287c2c2de1e0e40ad04e0e7a12
nwnordahl <nicolaiwnordahl@gmail.com>
2022-03-16 13:09:12 +0100

    Make function to play a round of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-function computerPlay() {
-  const playChoices = ["Rock", "Paper", "Scissors"];
-  const randomIndex = Math.floor(Math.random() * playChoices.length);
+// Global Variables
+const PLAYER_SELECTIONS = ["ROCK", "PAPER", "SCISSORS"];
 
-  return playChoices[randomIndex];
+// Helper functions
+function computerPlay() {
+  const randomIndex = Math.floor(Math.random() * PLAYER_SELECTIONS.length);
+
+  return PLAYER_SELECTIONS[randomIndex];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  const playerSelectionUpperCase = playerSelection.toUpperCase();
+
+  //   Check if computer win
+  if (
+    (playerSelectionUpperCase === "ROCK" && computerSelection === "PAPER") ||
+    (playerSelectionUpperCase === "PAPER" &&
+      computerSelection === "SCISSORS") ||
+    (playerSelectionUpperCase === "SCISSORS" && computerSelection === "ROCK")
+  ) {
+    return `You lose! ${computerSelection} beats ${playerSelectionUpperCase}`;
+  }
+
+  //   Check if player win
+  else if (
+    (computerSelection === "ROCK" && playerSelectionUpperCase === "PAPER") ||
+    (computerSelection === "PAPER" &&
+      playerSelectionUpperCase === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelectionUpperCase === "ROCK")
+  ) {
+    return `You win! ${playerSelectionUpperCase} beats ${computerSelection}`;
+  }
+
+  //   It's  draw
+  else {
+    return `Computer chose ${computerSelection} too, it's a draw!`;
+  }
+}
+
+// Test to see if it works
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
